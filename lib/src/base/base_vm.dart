7d1aaa6c43502acd1556337953527c655b0c2318
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2024-03-31 04:45:34 +0530

    feat: made bookings work

--- lib/src/base/base_vm.dart
+++ lib/src/base/base_vm.dart
@@ -71,14 +71,14 @@ class BaseVm extends ChangeNotifier {
      await Future.wait([
      bookingsVm.fetchTaxes(),
      yachtVm.fetchCharterOffers(),
-       fetchAllUsers(),
+    fetchAllUsers(),
      bookingsVm.fetchAppUrls(),
      settingsVm.fetchContent(),
      yachtVm.fetchServices(),
      yachtVm.fetchUserFavourites(),
      yachtVm.fetchCharters(),
      yachtVm.fetchYachts(),
-       homeVm.fetchAllBookings(),
+     homeVm.fetchAllBookings(),
      inboxVm.fetchNotificatoins(),
 
      ]);
--- lib/src/base/home/home_vm/home_vm.dart
+++ lib/src/base/home/home_vm/home_vm.dart
@@ -1,23 +1,23 @@
-
-
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:yacht_master/services/firebase_collections.dart';
 import 'package:yacht_master/src/base/search/view/bookings/model/bookings.dart';
 
-
 class HomeVm extends ChangeNotifier {
-  List<BookingsModel> allBookings=[];
+  List<BookingsModel> allBookings = [];
   StreamSubscription<List<BookingsModel>>? bookingsStream;
   Future<void> fetchAllBookings() async {
-    allBookings=[];
+    print("=============== Starting to fetch all bookings =================");
+    allBookings = [];
     try {
-      var ref = FbCollections.bookings
-          .snapshots()
-          .asBroadcastStream();
-      var res = ref.map((list) => list.docs.map((e) => BookingsModel.fromJson(e.data())).toList());
+      var ref = FbCollections.bookings.snapshots().asBroadcastStream();
+      var bookingsData = await FbCollections.bookings.get();
+      print(allBookings);
+      var res = ref.map((list) =>
+          list.docs.map((e) => BookingsModel.fromJson(e.data())).toList());
       bookingsStream ??= res.listen((bookings) async {
         if (bookings.isNotEmpty) {
           allBookings = bookings;
@@ -25,7 +25,6 @@ class HomeVm extends ChangeNotifier {
         }
         notifyListeners();
       });
-
 
       notifyListeners();
     } on Exception catch (e) {
@@ -35,8 +34,7 @@ class HomeVm extends ChangeNotifier {
     }
   }
 
-  update()
-  {
+  update() {
     notifyListeners();
   }
 }
