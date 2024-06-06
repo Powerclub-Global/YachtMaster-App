9f7c54aeec1a09cbca8e5205ebaace9285eabfed
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2024-06-07 02:36:40 +0530

    feat: made it work

--- lib/src/base/settings/view/about_app.dart
+++ lib/src/base/settings/view/about_app.dart
@@ -19,7 +19,7 @@ import '../../../../utils/launch_url.dart';
 import '../../../../utils/zbot_toast.dart';
 
 class AboutApp extends StatefulWidget {
-  static String route="/aboutApp";
+  static String route = "/aboutApp";
   @override
   _AboutAppState createState() => _AboutAppState();
 }
@@ -35,219 +35,238 @@ class _AboutAppState extends State<AboutApp> {
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
       ZBotToast.loadingShow();
-     await fetchSocialLinks();
+      await fetchSocialLinks();
       // await context.read<SettingsVm>().fetchContent();
 
       ZBotToast.loadingClose();
     });
     super.initState();
-
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: R.colors.black,
-      appBar: GeneralAppBar.simpleAppBar(context, getTranslated(context, "how_yachtmaster_works")??""),
-      body:  SingleChildScrollView(
+      appBar: GeneralAppBar.simpleAppBar(
+          context, getTranslated(context, "how_yachtmaster_works") ?? ""),
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child:  Column(
-                  children: [
-                    Container(
-                      padding:  EdgeInsets.symmetric(horizontal: Get.width*.04,vertical: Get.height*.008),
-                      margin: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: R.colors.blackLight,
-
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: Get.width * .04, vertical: Get.height * .008),
+                  margin: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: R.colors.blackLight,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(
+                        R.images.logo,
+                        height: Get.height * .07,
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Image.asset(
-                            R.images.logo,
-                            height: Get.height*.07,
-
-                          ),
-                          Text(
-                            'Version 1.1.1',
-                            style: R.textStyle.helveticaBold()
-                                .copyWith(color: R.colors.whiteDull,fontSize: 11.sp),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 1.h,
-                    ),
-                    Container(
-                      width: Get.width,
-                      margin: EdgeInsets.all(10),
-                      padding: const EdgeInsets.all(15),
-
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: R.colors.blackLight,
-
-                      ),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  getTranslated(context, 'website')??"",style: R.textStyle.helveticaBold().
-                                copyWith(fontSize: 12.sp,color: R.colors.whiteDull),
-                                ),
-                                Text(
-                                  websiteLink??"",
-                                  style: R.textStyle.helvetica()
-                                      .copyWith(color: R.colors.whiteDull,fontSize: 12.sp),
-                                ),
-                              ],
+                      Text(
+                        'Version 1.1.1',
+                        style: R.textStyle.helveticaBold().copyWith(
+                            color: R.colors.whiteDull, fontSize: 11.sp),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 1.h,
+                ),
+                Container(
+                  width: Get.width,
+                  margin: EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: R.colors.blackLight,
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              getTranslated(context, 'website') ?? "",
+                              style: R.textStyle.helveticaBold().copyWith(
+                                  fontSize: 12.sp, color: R.colors.whiteDull),
                             ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              Clipboard.setData(
-                                   ClipboardData(text:websiteLink??""));
-                              Helper.inSnackBar("Copied",
-                                  "Your text has been copied",R.colors.themeMud);
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.all(9),
-                              decoration: BoxDecoration(
-                                  color: R.colors.themeMud,
-                                  shape: BoxShape.circle),
-                              child: Icon(
-                                Icons.content_copy,
-                                color: R.colors.whiteColor,
-                                size: 20,
-                              ),
+                            Text(
+                              websiteLink ?? "",
+                              style: R.textStyle.helvetica().copyWith(
+                                  color: R.colors.whiteDull, fontSize: 12.sp),
                             ),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Clipboard.setData(
+                              ClipboardData(text: websiteLink ?? ""));
+                          Helper.inSnackBar("Copied",
+                              "Your text has been copied", R.colors.themeMud);
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(9),
+                          decoration: BoxDecoration(
+                              color: R.colors.themeMud, shape: BoxShape.circle),
+                          child: Icon(
+                            Icons.content_copy,
+                            color: R.colors.whiteColor,
+                            size: 20,
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 1.5.h,
-                    ),
-                    Container(
-                      width: Get.width,
-                      margin: EdgeInsets.all(10),
-                      padding:  EdgeInsets.symmetric(horizontal: Get.width*.04,vertical: Get.height*.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: R.colors.blackLight,
-
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 1.5.h,
+                ),
+                Container(
+                  width: Get.width,
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.symmetric(
+                      horizontal: Get.width * .04, vertical: Get.height * .02),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: R.colors.blackLight,
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        getTranslated(context, 'follow_us') ?? "",
+                        style: R.textStyle.helvetica().copyWith(
+                            fontSize: 14.sp, color: R.colors.whiteColor),
                       ),
-                      child: Column(
+                      SizedBox(
+                        height: 2.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Text(
-                            getTranslated(context, 'follow_us')??"",style: R.textStyle.helvetica().
-                          copyWith(fontSize: 14.sp,color: R.colors.whiteColor),
+                          // links(R.images.facebook,fb??"",),
+                          // links(R.images.google,google??"",),
+                          links(
+                            R.images.twitter,
+                            twitter ?? "",
                           ),
-                          SizedBox(
-                            height: 2.h,
+                          links(
+                            R.images.instagram,
+                            instagram ?? "",
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              links(R.images.facebook,fb??"",),
-                              links(R.images.google,google??"",),
-                              links(R.images.twitter,twitter??"",),
-                              links(R.images.instagram,instagram??"",),
-
-
-                            ],
-                          )
                         ],
-                      ),
-                    ),
-                    SizedBox(height: 2.h,),
-                    Column(children:List.generate(context.read<SettingsVm>().allContent.where((element) => element.type==AppContentType.howYachtWorks.index).length, (index) {
-                     ContentModel content= context.read<SettingsVm>().allContent.where((element) => element.type==AppContentType.howYachtWorks.index).toList()[index];
-                      return questionAnswer(content.title??"", content.content??"");
-                    }),)
-
-
-                  ],
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 2.h,
+                ),
+                Column(
+                  children: List.generate(
+                      context
+                          .read<SettingsVm>()
+                          .allContent
+                          .where((element) =>
+                              element.type ==
+                              AppContentType.howYachtWorks.index)
+                          .length, (index) {
+                    ContentModel content = context
+                        .read<SettingsVm>()
+                        .allContent
+                        .where((element) =>
+                            element.type == AppContentType.howYachtWorks.index)
+                        .toList()[index];
+                    return questionAnswer(
+                        content.title ?? "", content.content ?? "");
+                  }),
                 )
-        ),
+              ],
+            )),
       ),
     );
   }
-  Widget questionAnswer(String title,String desc)
-  {
-    return Consumer<SettingsVm>(
-        builder: (context, provider,_) {
-          return Padding(
-          padding:  EdgeInsets.only(bottom:Get.height*.02),
-          child: Container(
-            decoration: BoxDecoration(
+
+  Widget questionAnswer(String title, String desc) {
+    return Consumer<SettingsVm>(builder: (context, provider, _) {
+      return Padding(
+        padding: EdgeInsets.only(bottom: Get.height * .02),
+        child: Container(
+          decoration: BoxDecoration(
               color: R.colors.blackLight,
-              borderRadius: BorderRadius.circular(18)
-            ),
-            child: Column(children: [
+              borderRadius: BorderRadius.circular(18)),
+          child: Column(
+            children: [
               Container(
                 width: Get.width,
                 decoration: BoxDecoration(
                     color: R.colors.lightGrey,
-                    borderRadius: BorderRadius.circular(18)
-                ),
-                padding:  EdgeInsets.symmetric(horizontal: Get.width*.04,vertical: Get.height*.015),
+                    borderRadius: BorderRadius.circular(18)),
+                padding: EdgeInsets.symmetric(
+                    horizontal: Get.width * .04, vertical: Get.height * .015),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text(title,style: R.textStyle.helveticaBold().
-                  copyWith(color:R.colors.whiteColor ,fontSize: 12.5.sp,height: 1.2),),
+                  child: Text(
+                    title,
+                    style: R.textStyle.helveticaBold().copyWith(
+                        color: R.colors.whiteColor,
+                        fontSize: 12.5.sp,
+                        height: 1.2),
+                  ),
                 ),
               ),
               Padding(
-                padding:  EdgeInsets.symmetric(horizontal: Get.width*.04,vertical: Get.height*.02),
-                child: Text(desc.replaceAll("\\n", "\n"),style: R.textStyle.helvetica()
-                    .copyWith(color:R.colors.whiteDull ,fontSize: 11.5.sp,height: 1.2)),
+                padding: EdgeInsets.symmetric(
+                    horizontal: Get.width * .04, vertical: Get.height * .02),
+                child: Text(desc.replaceAll("\\n", "\n"),
+                    style: R.textStyle.helvetica().copyWith(
+                        color: R.colors.whiteDull,
+                        fontSize: 11.5.sp,
+                        height: 1.2)),
               ),
-            ],),
+            ],
           ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
-  Widget links(String img,String url)
-  {
+
+  Widget links(String img, String url) {
     return GestureDetector(
-      onTap: ()
-      {
+      onTap: () {
         LaunchUrl.launchURL(url);
       },
       child: Image.asset(
         img,
-        height: Get.height*.045,
+        height: Get.height * .045,
       ),
     );
   }
-
 
   Future<void> fetchSocialLinks() async {
     try {
       QuerySnapshot snapshot = await FbCollections.appSocialLinks.get();
       if (snapshot.docs.isNotEmpty) {
-       websiteLink=snapshot.docs.first.get("website");
-       fb=snapshot.docs.first.get("facebook");
-       google=snapshot.docs.first.get("google");
-       twitter=snapshot.docs.first.get("twitter");
-       instagram=snapshot.docs.first.get("instagram");
-       setState(() {
-
-       });
+        websiteLink = snapshot.docs.first.get("website");
+        fb = snapshot.docs.first.get("facebook");
+        google = snapshot.docs.first.get("google");
+        twitter = snapshot.docs.first.get("twitter");
+        instagram = snapshot.docs.first.get("instagram");
+        setState(() {});
       }
     } catch (e) {
       debugPrintStack();
       log(e.toString());
     }
   }
-
 }
